8c35c7b274eabdb7e8a1052583fc633b8b4d4b01
MaroulisManolis <manmaroulis@gmail.com>
2023-03-21 09:14:02 +0200

    Learning about Property and Event Bindings

--- first-project/src/app/servers/servers.component.ts
+++ first-project/src/app/servers/servers.component.ts
@@ -4,11 +4,28 @@ import { Component } from '@angular/core';
   // selector: '[app-servers]',
   // selector: '.app-servers',
   selector: 'app-servers',
-  template: `
-      <app-server></app-server>
-      <app-server></app-server>`,
+  // template: `
+  //     <app-server></app-server>
+  //     <app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  ngOnInit() {
+
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created!';
+  }
 
 }
