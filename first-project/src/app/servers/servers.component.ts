0ea203d0cc41ce602497c73aaaad80ef5d43de7c
MaroulisManolis <manmaroulis@gmail.com>
2023-03-23 08:51:22 +0200

    Learning about directives (ngIf, ngStyle, ngClass & ngFor)

--- first-project/src/app/servers/servers.component.ts
+++ first-project/src/app/servers/servers.component.ts
@@ -14,6 +14,8 @@ export class ServersComponent {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName = 'Testserver';
+  serverCreated = false;
+  servers = ['Testserver', 'Testserver 2'];
 
   constructor() {
     setTimeout(() => {
@@ -26,6 +28,8 @@ export class ServersComponent {
   }
 
   onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
 
