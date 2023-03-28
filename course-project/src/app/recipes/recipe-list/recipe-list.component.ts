cc830be589060047e874cd903b4031519356fc58
MaroulisManolis <manmaroulis@gmail.com>
2023-03-28 10:14:52 +0300

    adding my first recipe

--- course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+import { Recipe } from '../recipe.model'
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe('Souvlaki', 'This is a Greek delicacy', 'https://www.poupadou.com/blog/wp-content/uploads/2020/04/Souvlaki-aux-brochettes-de-poulet-pain-pita-frites-tzatziki-et-tomates-1-1018x679.jpg')
+  ];
+
 
 }
